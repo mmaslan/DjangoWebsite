e9240050b2024602badacf7da2a2e0708d8fd966
mmaslan <marek.maslany1@gmail.com>
2022-08-21 15:37:17 +0200

    Update admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Subject, Course, Module
 
-# Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModelInLine(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(Course):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModelInLine]
