1ef12741aa584a49081efbc2bab42a44d1d98e1e
mmaslan <marek.maslany1@gmail.com>
2022-08-29 16:49:39 +0200

    Static files

--- DjangoWebsite/settings.py
+++ DjangoWebsite/settings.py
@@ -117,12 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "courses/static"),
+    BASE_DIR / "static",
 ]
-
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
