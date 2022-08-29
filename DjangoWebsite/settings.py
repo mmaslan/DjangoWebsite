67971c8b4c6cbd8e534e94a774fa841df8cae190
mmaslan <marek.maslany1@gmail.com>
2022-08-29 16:30:45 +0200

    Static folder update

--- DjangoWebsite/settings.py
+++ DjangoWebsite/settings.py
@@ -120,9 +120,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "blog/static"),
+    os.path.join(BASE_DIR, "courses/static"),
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- DjangoWebsite/urls.py
+++ DjangoWebsite/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-
 ]
+
+urlpatterns += staticfiles_urlpatterns()
