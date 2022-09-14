80b0dc9b36f775e76963cef821298a43ae5ce106
mmaslan <marek.maslany1@gmail.com>
2022-09-14 18:49:14 +0200

    urls.py update

--- DjangoWebsite/urls.py
+++ DjangoWebsite/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import (
+    path,
+    include
+)
 
 
 urlpatterns = [
