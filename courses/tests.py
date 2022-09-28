d8bc850be7e2354848a321e664ec893cb478a363
mmaslan <marek.maslany1@gmail.com>
2022-09-28 22:13:02 +0200

    Tests

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class URLTest(TestCase):
+    def test_base(self):
+        response = self.client.get('/base.html')
+        self.assertTrue(response, 200)
+
+    def test_login(self):
+        response = self.client.get('/login.html')
+        self.assertTrue(response, 200)
+
+    def test_logged_out(self):
+        response = self.client.get('/logged_out.html')
+        self.assertTrue(response, 200)
