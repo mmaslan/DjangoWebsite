29d1001a1ecfd309b1395291090f4e0d88b4e6f0
mmaslan <marek.maslany1@gmail.com>
2022-09-29 22:21:59 +0200

    Tests

--- courses/tests.py
+++ courses/tests.py
@@ -13,3 +13,27 @@ class URLTest(TestCase):
     def test_logged_out(self):
         response = self.client.get('/logged_out.html')
         self.assertTrue(response, 200)
+
+    def test_detail(self):
+        response = self.client.get('/detail.html')
+        self.assertTrue(response, 200)
+
+    def test_list(self):
+        response = self.client.get('/list.html')
+        self.assertTrue(response, 200)
+
+    def test_file(self):
+        response = self.client.get('/file.html')
+        self.assertTrue(response, 200)
+
+    def test_image(self):
+        response = self.client.get('/image.html')
+        self.assertTrue(response, 200)
+
+    def test_text(self):
+        response = self.clinet.get('/text.html')
+        self.assertTrue(response, 200)
+
+    def test_video(self):
+        response = client.get('/video.html')
+        self.assertTrue(response, 200)
